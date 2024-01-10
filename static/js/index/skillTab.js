3a62ac657c2314345178f0526b848b6fc22c393f
Gints <gintskristaps13@gmail.com>
2024-01-10 17:56:16 +0200

    Add workflow

--- static/js/index/skillTab.js
+++
@@ -1,7 +0,0 @@
-values = document.getElementsByClassName("skillValue");
-for (const value of values) {
-    let v = value.textContent.trim();
-    let progress = value.getElementsByClassName("progress")[0];
-    progress.style.width = v + "%";
-
-}
